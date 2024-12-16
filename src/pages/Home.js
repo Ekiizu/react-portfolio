fbbbe8ab7278dc29f83c5b364300f2bcf19227b8
Emma Ann Hynes <ekiizu2003@gmail.com>
2024-12-16 00:31:05 +0000

    moved to home pc

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,17 @@
+import React from 'react';
+import '../index.css'; // Make sure to import your custom CSS if you're using an external CSS file
+
 const Home = () => {
     return (
-        <h1>Hello from Home Page!!! (˶˃ᆺ˂˶)</h1>
-
-        
+        <div className="home-container">
+            <h1>Hello from Home Page!!! (˶˃ᆺ˂˶)</h1>
+            <img 
+                src="https://media.tenor.com/MWIAxMsqaGUAAAAi/glorp-gets-what-it-deserves.gif" 
+                alt="Gif" 
+                className="centered-gif" 
+            />
+        </div>
     );
 };
-
 
 export default Home;
--- src/pages/projects/Index.js
+++ src/pages/projects/Index.js
@@ -1,7 +1,17 @@
 import { Link } from "react-router-dom";
 import ProjectCard from "../../components/ProjectCard";
 
-const Index = () => { //uh oh something broken
+const Index = () => {
+    // static list of projects
+    const projects = [
+        { id: 1, name: "Project One", description: "Description of Project One" },
+        { id: 2, name: "Project Two", description: "Description of Project Two" },
+        { id: 3, name: "Project Three", description: "Description of Project Three" },
+        //BROKEN
+    ];
+
+    // Filter the list if needed 
+    const filteredList = projects.filter((project) => project.name.toLowerCase().includes("project"));
 
     const projectCards = filteredList.map((project, index) => {
         return <ProjectCard key={project.id} project={project} />;
